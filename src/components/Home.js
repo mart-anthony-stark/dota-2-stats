b677ffaab85767366738e0824b83451c908f9ac6
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-01 15:43:58 -0700

    1 file modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Home(props) {
   const [heroes, setHeroes] = useState([{ roles: [] }]);
 
   async function getData() {}

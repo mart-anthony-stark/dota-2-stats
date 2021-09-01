4c28126c9bd313d040728a79f7b84b9609f3b1b9
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 20:36:29 -0700

    3 files modified

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,19 @@ import '../styles/navigation.css';
 export default function Nav() {
   return (
     <nav>
+      <div className="logo">Dota Talk</div>
       <div className="links">
         <Link to="/">
           <button>Home</button>
         </Link>
         <Link to="/about">
           <button>About</button>
+        </Link>
+        <Link to="/heroes">
+          <button>Heroes</button>
+        </Link>
+        <Link to="/teams">
+          <button>Teams</button>
         </Link>
         <Link to="/heroes">
           <button>Heroes</button>

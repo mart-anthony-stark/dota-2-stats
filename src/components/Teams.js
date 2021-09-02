ec567c1c10fc44ee5d362d9d28a8eee07c5e7fc9
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-01 21:25:04 -0700

    4 files modified

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -2,9 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Teams() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [teams, setTeams] = useState([]);
+
+  const getData = async () => {
+    const res = await fetch('https://api.opendota.com/api/teams');
+    const data = await res.json();
+    const filtered = data.slice(0, 10).filter(team => team.name !== '');
+    setTeams(filtered);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <h2>Teams</h2>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        teams.map(team => (
+          <div key={team.team_id}>
+            <h3>{team.name}</h3>
+            <img src={team.logo_url} alt="" />
+          </div>
+        ))
+      )}
     </div>
   );
 }

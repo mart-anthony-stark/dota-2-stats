2fb4d2a79cd20affa7af804e669c20fcbb78305f
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-03 14:06:11 -0700

    1 file modified

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -5,25 +5,36 @@ import LoadingText from '../shared/loadingText';
 export default function Teams() {
   const [isLoading, setIsLoading] = useState(true);
   const [teams, setTeams] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [teamsPerPage, setTeamsPerPage] = useState(10);
 
   const getData = async () => {
     const res = await fetch('https://api.opendota.com/api/teams');
     const data = await res.json();
     const filtered = data.slice(0, 10).filter(team => team.name !== '');
-    setTeams(filtered);
+    setTeams(data);
     setIsLoading(false);
   };
 
   useEffect(() => {
     getData();
   }, []);
+
+  // PAGINATION
+  const endIndex = currentPage * teamsPerPage;
+  const startIndex = endIndex - teamsPerPage;
+  const currentData = teams.slice(startIndex, endIndex);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div>
       <h2>Teams</h2>
       {isLoading ? (
         <LoadingText />
       ) : (
-        teams.map(team => (
+        currentData.map(team => (
           <div key={team.team_id}>
             <h3>{team.name}</h3>
             <img src={team.logo_url} alt="" />

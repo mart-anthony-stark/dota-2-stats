739d19f36bb1f6fd3572835181779db4435a872a
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-07 21:58:25 +0800

    1 file modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/home.css';
+import Aos from 'aos'
 
 export default function Home(props) {
   const [heroes, setHeroes] = useState([{ roles: [] }]);
 
   async function getData() {}
   useEffect(() => {
+    Aos.init({})
     getData();
   }, []);
   return (
@@ -24,8 +26,8 @@ export default function Home(props) {
       </div>
       <section>
         <div className="lines">
-          <h2 className="join">JOIN THE BATTLE OF THE ANCIENTS</h2>
-          <p>
+          <h2 className="join" data-aos="fade-up" data-aos-duration="2500">JOIN THE BATTLE OF THE ANCIENTS</h2>
+          <p data-aos="fade-up" data-aos-duration="2500">
             Every day, millions of players worldwide enter the battle as one of
             over a hundred Dota Heroes in a 5v5 team clash. Dota is the deepest
             multi-player action RTS game ever made and there's always a new

7eb791c3b5c33541aa662daa9d8eddad9d9f13ac
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-07 22:13:07 +0800

    3 files modified

--- src/components/HeroDetails.js
+++ src/components/HeroDetails.js
@@ -12,49 +12,53 @@ export default function HeroDetails({ match, location }) {
   }, []);
   return (
     <div className="hero-details comp">
-      <Link to="/">Back</Link>
-      {hero.localized_name}
-      <img
-        className="hero-image"
-        src={`https://api.opendota.com${hero.img}`}
-        alt=""
-      />
-      <p>Primary attribute: {hero.primary_attr}</p>
-      <p>Roles: {hero.roles}</p>
+      <Link to="/heroes" className="back">
+        <span class="iconify" data-icon="mdi:arrow-left-circle" />
+      </Link>
+      <div className="card">
+        {hero.localized_name}
+        <img
+          className="hero-image"
+          src={`https://api.opendota.com${hero.img}`}
+          alt=""
+        />
+        <p>Primary attribute: {hero.primary_attr}</p>
+        <p>Roles: {hero.roles}</p>
 
-      {/* <p>Base Health: {hero.base_health}</p> */}
-      <div className="bar-container">
-        <div
-          className="bar health"
-          style={{ width: (hero.base_health / 500) * 100 + 200 }}
-        >
-          {hero.base_health} HP
+        {/* <p>Base Health: {hero.base_health}</p> */}
+        <div className="bar-container">
+          <div
+            className="bar health"
+            style={{ width: (hero.base_health / 500) * 100 + 200 }}
+          >
+            {hero.base_health} HP
+          </div>
         </div>
-      </div>
 
-      {/* <p>Base Mana: {hero.base_mana}</p> */}
-      <div className="bar-container">
-        <div
-          className="bar mana"
-          style={{ width: (hero.base_mana / 500) * 100 + 200 }}
-        >
-          {hero.base_mana} MP
+        {/* <p>Base Mana: {hero.base_mana}</p> */}
+        <div className="bar-container">
+          <div
+            className="bar mana"
+            style={{ width: (hero.base_mana / 500) * 100 + 200 }}
+          >
+            {hero.base_mana} MP
+          </div>
         </div>
-      </div>
 
-      <p>Base Strength{hero.base_str}</p>
-      <p>Base Agility: {hero.base_agi}</p>
-      <p>Base Intelligence: {hero.base_int}</p>
-      <p>
-        Base Attack: {hero.base_attack_min} - {hero.base_attack_max}
-      </p>
-      <p>Base Str Gain: {hero.str_gain}</p>
-      <p>Base Agi Gain: {hero.agi_gain}</p>
-      <p>Base Int Gain: {hero.int_gain}</p>
-      <p>Attack Range: {hero.attack_range}</p>
-      <p>Projectile Speed: {hero.projectile_speed}</p>
-      <p>Attack Rate: {hero.attack_rate}</p>
-      <p>Move Speed: {hero.move_speed}</p>
+        <p>Base Strength{hero.base_str}</p>
+        <p>Base Agility: {hero.base_agi}</p>
+        <p>Base Intelligence: {hero.base_int}</p>
+        <p>
+          Base Attack: {hero.base_attack_min} - {hero.base_attack_max}
+        </p>
+        <p>Base Str Gain: {hero.str_gain}</p>
+        <p>Base Agi Gain: {hero.agi_gain}</p>
+        <p>Base Int Gain: {hero.int_gain}</p>
+        <p>Attack Range: {hero.attack_range}</p>
+        <p>Projectile Speed: {hero.projectile_speed}</p>
+        <p>Attack Rate: {hero.attack_rate}</p>
+        <p>Move Speed: {hero.move_speed}</p>
+      </div>
     </div>
   );
 }
